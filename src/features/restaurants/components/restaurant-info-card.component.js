09e123cf68e095e06d25813b0197cf57a7e3fc76
lastsaintcrucified <lastsaintcrucified@gmail.com>
2022-11-23 22:46:55 +0600

    spacer

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,36 +1,91 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View, Image } from "react-native";
 import { Card } from "react-native-paper";
 import styled from "styled-components/native";
-
+import { SvgXml } from "react-native-svg";
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 const RestaurantCardCover = styled(Card.Cover)`
-  padding: 10px;
+  padding: ${(props) => props.theme.space[1]};
   background-color: ${(props) => props.theme.colors.bg.primary};
 `;
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[2]};
+`;
 const Title = styled.Text`
-  padding: 10px;
-  font-size: 17px;
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.title};
+`;
+const Address = styled.Text`
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+const Rating = styled.View`
+  flex: 5;
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[1]};
+  padding-bottom: ${(props) => props.theme.space[1]};
+`;
+const Opening = styled.View`
+  flex: 5;
+  flex-direction: row-reverse;
+  padding-top: ${(props) => props.theme.space[1]};
+  padding-bottom: ${(props) => props.theme.space[1]};
+`;
+const SvgRow = styled.View`
+  flex-direction: row;
+  align-content: space-between;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
 `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     isOpenNow = true,
     photos = [
       "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
     ],
     address = "Some street behind there",
     rating = 4,
-    isClosedTemporarily,
+    isClosedTemporarily = true,
   } = restaurant;
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
   return (
     <RestaurantCard elevation={2}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
-      <Title>{name}</Title>
+      <Info>
+        <Title>{name}</Title>
+        <SvgRow>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <Opening>
+            {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            <View style={{ paddingLeft: 10 }} />
+            {isClosedTemporarily && (
+              <>
+                <Image
+                  style={{ width: 15, height: 15 }}
+                  source={{ uri: icon }}
+                />
+                <View style={{ paddingLeft: 10 }} />
+                <Text variant="label" style={{ color: "red", fontSize: 10 }}>
+                  CLOSED TEMPORARILY
+                </Text>
+              </>
+            )}
+          </Opening>
+        </SvgRow>
+
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   );
 };
